d518adfc05fe67b0568ce74e6dc2ab314601c093
squarezy <squarezzzy@gmail.com>
2022-03-28 16:38:48 +0100

    created model func for get api/articles/:article_id

--- models/models.js
+++ models/models.js
@@ -4,3 +4,15 @@ exports.getTopics = async () => {
   const results = await db.query("SELECT * FROM topics");
   return results.rows;
 };
+
+exports.getArticle = async (articleID) => {
+  articleID = Number(articleID);
+  const results = await db.query("SELECT * FROM articles");
+
+  const articleArr = results.rows.filter((article) => {
+    if (article.article_id === articleID) {
+      return article;
+    }
+  });
+  return articleArr[0];
+};
