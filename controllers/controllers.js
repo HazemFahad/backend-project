caffdc53243ca5195d21fe5cee1a0ca13331e1ba
squarezy <squarezzzy@gmail.com>
2022-03-29 15:54:50 +0100

    Updates controllers for GET usernames

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,5 +1,10 @@
 const res = require("express/lib/response");
-const { getTopics, getArticle, updateVotes } = require("../models/models");
+const {
+  getTopics,
+  getArticle,
+  updateVotes,
+  getAllUsers,
+} = require("../models/models");
 
 exports.getTopics = async (req, res, next) => {
   try {
@@ -26,7 +31,16 @@ exports.patchArticleById = async (req, res, next) => {
     const voteNum = req.body.inc_votes;
 
     const article = await updateVotes(articleID, voteNum);
-    res.status(200).send({ article }); ///do this for rest
+    res.status(200).send({ article });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.getUsernames = async (req, res, next) => {
+  try {
+    const users = await getAllUsers();
+    res.status(200).send({ users });
   } catch (err) {
     next(err);
   }
